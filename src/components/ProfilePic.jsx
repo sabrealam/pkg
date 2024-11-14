29a69b8ca3a08a7d7b3da43bcc291697c4aeafcc
sabre <asabre5073@gmail.com>
2024-11-14 23:01:54 +0530

    Added

---
+++ src/components/ProfilePic.jsx
@@ -0,0 +1,10 @@
+
+function ProfilePic({ profilePic }) {
+    return (
+        <div className="profile-pic">
+            <img src={profilePic} alt="Profile Pic" />
+        </div>
+    );  
+}
+
+export default ProfilePic
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as ProfilePic } from './components/ProfilePic';
